e47e1c30d6410a8a57316c3000dc0642b520e602
Maxi <maxi.kapko73@gmail.com>
2020-09-18 20:57:10 -0300

    Agrego el login y register

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
 
+from django.contrib import admin
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.login),
+    path('register', views.register),
+    path('login', views.login),
+    path('logout', views.logout),
+
+
 ]
