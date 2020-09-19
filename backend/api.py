1581d437bedb8059bbf57d039de9f92d14b49e8b
Maxi <maxi.kapko73@gmail.com>
2020-09-19 18:40:30 -0300

    Bug Fix de tipeo

--- backend/api.py
+++ backend/api.py
@@ -4,10 +4,10 @@ from rest_framework.views import APIView
 from rest_framework import status
 
 class UserAPI(APIView):
-    def post(self,reuqest):
+    def post(self,request):
         serializer = UserSerializer( data = request.data)
         if serializer.is_valid():
-            user = serializert.save()
+            user = serializer.save()
             return Response(serializer.data, status = status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
