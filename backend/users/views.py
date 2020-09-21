396d03831e757ddc4c6dd9238179d78ddfa7bcc1
BealeStBluesBoy <gustavo.cargnelutti@outlook.com>
2020-09-21 17:07:11 -0300

    Remover dependencia de la app users con backend

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from backend.users import serializers
+from . import serializers
 
 class RegisterRequest(APIView):
     def post(self, request):
