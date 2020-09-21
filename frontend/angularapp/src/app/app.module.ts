5ddaeb6da64a1c83b218d07f82ec907dc704e8ce
SoraireEmmanuel <59460182+SoraireEmmanuel@users.noreply.github.com>
2020-09-20 23:38:07 -0300

    Feature reserva frontend (#5)
    * git
    
    * frontend busqueda carpa y sombrilla

--- frontend/angularapp/src/app/app.module.ts
+++ frontend/angularapp/src/app/app.module.ts
@@ -3,11 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { ReservaComponent } from './componenet/reserva/reserva.component';
+
 import { NavbarComponent } from './componenet/navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    ReservaComponent,
     NavbarComponent
   ],
   imports: [
