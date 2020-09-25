3d29c5127070aa6588aff48e3a9ed95cc4b85f5a
SoraireEmmanuel <59460182+SoraireEmmanuel@users.noreply.github.com>
2020-09-25 00:09:25 -0300

    Creacion de servicio, validacion de formulario desde el ngModel y creacion de la clase UsuarioModel

--- frontend/angularapp/src/app/componenet/navbar/navbar.component.ts
+++ frontend/angularapp/src/app/componenet/navbar/navbar.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
+import { UsuarioModel} from '../../models/usuario.model'
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+usuario: UsuarioModel;
+passwordConfirm: string;
+usuariologin:UsuarioModel;
   constructor() { }
 
   ngOnInit(): void {
+    this.usuario= new UsuarioModel;
+    this.usuariologin= new UsuarioModel;
   }
 
+  onSubmit(formulario: NgForm){
+    console.log('formulario enviado');
+    console.log(this.usuario);
+    console.log(formulario); 
+  }
+
+  onSubmitLogin(formulario: NgForm){
+    
+    console.log('formulario enviado');
+    console.log(this.usuariologin);
+    console.log(formulario); 
+  }
 }
---
+++ frontend/angularapp/src/app/componenet/respuesta-creacion-cuenta/respuesta-creacion-cuenta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-respuesta-creacion-cuenta',
+  templateUrl: './respuesta-creacion-cuenta.component.html',
+  styleUrls: ['./respuesta-creacion-cuenta.component.css']
+})
+export class RespuestaCreacionCuentaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
