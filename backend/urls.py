a4ed3d3ab09e6fe01419f2240230b9338187e882
santiago <santiagolozzia@gmail.com>
2020-10-02 20:03:06 -0300

    agrego endpoint para mandar email e imprimir en consola

--- backend/urls.py
+++ backend/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.TestRequest.as_view()),
+    path('email/', views.EmailRequest.as_view()),
     path('auth/', include('backend.users.urls')),
 ]
--- backend/views.py
+++ backend/views.py
@@ -1,8 +1,25 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.core.mail import send_mail
+
 
 class TestRequest(APIView):
     def get(self, request):
+        return Response({
+            "test": "data"
+        })
+
+
+class EmailRequest(APIView):
+    def post(self, request):
+        send_mail(
+            'Subject here',
+            'Here is the message.',
+            'from@example.com',
+            # [request.user.email],
+            ['from@example.com'],
+            fail_silently=False,
+        )
         return Response({
             "test": "data"
         })
