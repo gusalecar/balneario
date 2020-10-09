f4335eaff7a7899537e5f062450d1c9d73f0cc5d
Gustavo Cargnelutti <gustavo.cargnelutti@outlook.com>
2020-10-08 21:22:46 -0300

    API para realizar reservas (#18)
    * Crear API para realizar reservas
    
    * Solo para usuarios autenticados
    * Permite ver solo las reservas propias
    * Chequea disponibilidad, existencia y superposicion
    
    * Version anterior: https://github.com/BealeStBluesBoy/balneario/pull/16
    
    * Deshabilitar R0201 en pylint
    
    * Agregar fixture con items reservables
    
    * Agregar endpoint para acceder a los items disponibles
    
    * Con opcion de filtrar por numero, tipo y habilitacion
    * El parametro habilitado es true por defecto
    
    * Arreglar error en el parametro habilitado de items
    
    * Agregar chequeo de items por fecha
    
    * Parametros: fechainicio y fechafin
    * Formato fechas: YYYY-MM-DD

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,13 @@
 from django.urls import include, path
-
+from rest_framework import routers
 from . import views
+
+router = routers.DefaultRouter()
+router.register(r'reservas', views.ReservaViewSet, 'reservas')
+router.register(r'items', views.ItemViewSet, 'items')
 
 urlpatterns = [
     path('', views.TestRequest.as_view()),
     path('auth/', include('backend.users.urls')),
+    path('', include(router.urls))
 ]
--- backend/views.py
+++ backend/views.py
@@ -1,8 +1,45 @@
+from rest_framework import viewsets, status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from .models import Item, Reserva
+from .serializers import ItemSerializer, ReservaSerializer
 
 class TestRequest(APIView):
     def get(self, request):
         return Response({
             "test": "data"
         })
+
+class ReservaViewSet(viewsets.ModelViewSet):
+    serializer_class = ReservaSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Reserva.objects.filter(usuario=self.request.user)
+
+    def create(self, request):
+        reserva = self.serializer_class(data=request.data)
+        if reserva.is_valid():
+            reserva.save(usuario=request.user)
+            return Response({ 'success': True })
+        return Response(reserva.errors, status.HTTP_400_BAD_REQUEST)
+
+class ItemViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = ItemSerializer
+
+    def get_queryset(self):
+        queryset = Item.objects.all()
+        params = self.request.query_params
+        if params.get('tipo'):
+            queryset = queryset.filter(tipo=params['tipo'])
+        if params.get('numero'):
+            queryset = queryset.filter(numero=params['numero'])
+        if params.get('habilitado'):
+            if params['habilitado'].capitalize() in [ 'True', 'False' ]:
+                queryset = queryset.filter(habilitado=params['habilitado'].capitalize())
+        if (params.get('fechainicio') is not None) & (params.get('fechafin') is not None):
+            queryset = queryset.filter(
+                detalles__fecha_fin__lte=params['fechafin'],
+                detalles__fecha_inicio__gte=params['fechainicio']
+            )
+        return queryset
