9042917e13ab89df07b3c28307b2b17005c3c42c
SoraireEmmanuel <59460182+SoraireEmmanuel@users.noreply.github.com>
2020-10-10 04:17:45 -0300

    mostrar reservas en tabla

--- frontend/angularapp/src/app/componenet/listareservas/listareservas.component.ts
+++ frontend/angularapp/src/app/componenet/listareservas/listareservas.component.ts
@@ -7,11 +7,21 @@ import { AuthService } from '../../services/auth.service'
   styleUrls: ['./listareservas.component.css']
 })
 export class ListareservasComponent implements OnInit {
-
+reservas:any;
+file:File;
   constructor(private auth:AuthService) { }
 
   ngOnInit(): void {
-    this.auth.verMisReservas().subscribe(resp=>{console.log(resp)})
+    this.auth.verMisReservas().subscribe(resp=>{
+      console.log(resp);
+      this.reservas=resp;})
+  }
+  fileEvent(fileInput:Event){
+    this.file=(<HTMLInputElement>fileInput.target).files[0];
+  }
+  subirArchivo(){
+    this.auth.subirArchivo(this.file).subscribe(res=>{console.log(res);});
+
   }
 
 }
